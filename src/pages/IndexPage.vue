e66a2f2db2ea4c5d799a95fd6f32ded79e299402
Seth Morton <seth.m.morton@gmail.com>
2022-12-04 20:58:20 -0800

    Update the page text

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,28 +1,69 @@
 <template>
   <q-page class="row">
     <q-card class="col-4 q-ma-xl">
-      "Secret Santa" is a gift-giving game wherin each participant (the giver)
-      is assigned the name of another participant (the reciever), and the giver
-      purchases a gift for the reciever. Each person gets a gift! For large
-      families or work settings, it is a good way to share gifts without making
-      people feel as though they must spend a lot of time and money purchasing
-      gifts for everyone.
-      <br />
-      <br />
-      Traditionally, at some point ahead of the gathering, names are placed into
-      a hat and each participant draws for whom they will be purchasing.
-      However, there are some times when getting everyone together is not
-      feasable (for example, in a global pandemic), so this program serves as a
-      virtual "hat". To be fair - this program does not generate secret results.
-      At least the person executing the program will see all the assignments...
-      <br />
-      <br />
-      However, in practice, it is often desirable that people that are very
-      close (e.g. spouses, significant others, etc.) not get each other in the
-      Secret Santa because they were already buying each other gifts. To solve
-      this problem you can assign a list of "invalid" recipients for each
-      partipant. The `secret-santa` program will not allow these people to be
-      recipients for that participant.
+      <q-card-section>
+        <div class="text-h6">What is Secret Santa?</div>
+      </q-card-section>
+      <q-card-section>
+        "Secret Santa" is a gift-giving game wherin each participant (the giver)
+        is assigned the name of another participant (the reciever), and the
+        giver purchases a gift for the reciever. Each person gets a gift! For
+        large families or work settings, it is a good way to share gifts without
+        making people feel as though they must spend a lot of time and money
+        purchasing gifts for everyone.
+      </q-card-section>
+      <q-card-section>
+        Traditionally, at some point ahead of the gathering, names are placed
+        into a hat and each participant draws for whom they will be purchasing.
+        However, there are some times when getting everyone together is not
+        feasable (for example, in a global pandemic), so this program serves as
+        a virtual "hat".
+      </q-card-section>
+      <q-card-section>
+        To be fair - this program does not generate secret results... at a
+        minimum the person executing the program will see all the assignments.
+      </q-card-section>
+      <q-separator></q-separator>
+      <q-card-section>
+        <div class="text-h6">Instructions</div>
+        <ol>
+          <li>
+            Add the names of all the participants in the Secret Santa by typing
+            thier name into the "Add a Participant" field and pressing "Enter".
+          </li>
+          <ul>
+            <li>
+              If at any time you need to remove a name, press the red "Trash"
+              icon to the right of thier name to remove them.
+            </li>
+          </ul>
+          <li>
+            (<strong>Optional</strong>) You can specify which recipients are
+            valid for each gifter by pressing the down arrow to the right of
+            each name and checking or unchecking names - checked names are
+            possible recipients.
+            <ul>
+              <li>
+                This can be used to prevent people that are very close (e.g.
+                spouses, significant others, etc.) from getting each other in
+                the Secret Santa, which could be a problem because they were
+                already buying each other gifts.
+              </li>
+            </ul>
+          </li>
+          <li>
+            Press the green "Solve The Secret Santa Problem" to see the solution
+            for your Secret Santa criteria.
+          </li>
+          <ul>
+            <li>
+              The solution will be presented in a table on the right side of the
+              screen.
+            </li>
+          </ul>
+        </ol>
+        You can start over at any time by pressing the red "Start Over" button.
+      </q-card-section>
     </q-card>
     <q-card class="col-3 q-ma-xl">
       <ParticipantInput
